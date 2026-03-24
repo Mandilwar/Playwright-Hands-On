5af48c4fc269a092a0bfecb9ff3269f47edfc983
Umang <umang.mandilwar@lilly.com>
2026-03-24 10:36:09 +0530

    Small Modification on code

--- tests/UIbasicstest.spec.js
+++ tests/UIbasicstest.spec.js
@@ -1,6 +1,7 @@
 const {test} = require ('@playwright/test');
 test('Browser Context Playwright Test',async ({browser})=>
     {
+        chrome - plugins
         const context = await browser.newContext();
         const page = await context.newPage();
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
